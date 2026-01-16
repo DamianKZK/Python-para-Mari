d9634acba300293560d26f2a117bafee04e0fd39
DamianKZK <damian.contreras.pizza@gmail.com>
2026-01-15 21:24:04 -0600

    Archivos hasta Bucles

---
+++ Operadores.py
@@ -0,0 +1,39 @@
+
+a = 8
+b = 12
+
+#OPERADORES ARITMETICOS
+
+print(a+b)  #Suma
+print(a-b)  #Resta
+print(a*b)  #Multiplicación
+print(a/b)  #División
+print(a**b) #Exponente
+print(a%b)  #Modulo
+
+
+#OPERADORES DE COMPARACIÓN
+print(f"{a}<{b} {a<b}")  #Menor que
+print(a>b)  #Mayor que
+print(a<=b) #Menor o igual que
+print(a>=b) #Mayor o igual que
+print(a==b) #Exactamente igual que 
+print(a!=b) #Distinto que 
+
+
+c = True
+d = False
+
+#OPERADORES LOGICOS
+
+print (c and d)
+print (c or d)
+print (not c)
+print (not d)
+
+#Condicionales
+
+if a<=b and a!=b:
+    print("Es verdad ")
+else:
+    print("Es falso")
